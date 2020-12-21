aa555a373fa9f495515117f6ce1613152cf39c1c
gignerK309 <dlatldydid18@naver.com>
2020-12-21 17:57:38 +0900

    make routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-import React from 'react';
-import axios from "axios";
-import Movie from "./movies";
+import React from "react";
+import {  HashRouter,Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import "./app.css";
 
-class App extends React.Component{
-  
-  state = {
-    isLoading: true,
-    movies:[]
-  };
-  getMovies= async () => {
-    const { data:
-      { data:
-        { movies }
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    this.setState({ movies , isLoading: false});
+function App() {
+  return(
+  <HashRouter>
+    <Navigation></Navigation>
+    <Route path="/" exact={true} component={Home}></Route>
+    <Route path ="/about" component={About} ></Route>
+    <Route path ="/movie/:id" component={Detail} ></Route>
+    </HashRouter>
+  );
 }
-   componentDidMount() {
-     this.getMovies();
-  }
-  render() {
-    const { isLoading ,movies} = this.state;
-    return (<section className="container">
-      {isLoading ?
-        (<div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>)
-        : (
-          <div className="movies">
-            {movies.map(
-              movie => {
-            return <Movie key={movie.id}
-              id={movie.id}
-              title={movie.title}
-              genres={movie.genres}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              year={movie.year} />
-          })}
-          </div>
-        )}
-    </section>
-    );
-  }
-}
-
-
-
-
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./detail.css";
+
+class Detail extends React.Component{
+    componentDidMount() {
+        console.log(this.props)
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push("/");
+        }
+    }
+    render() {
+        const { location } = this.props;
+        if (location.state) {
+            return (<div>
+                  <div className="detail">
+                 <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+              <div className="detail__data" >
+                 <h3 className="detail__title" >{location.state.title}</h3>
+                 <h5 className="detail__year">{location.state.year}</h5>
+                 <ul className ="detail__genres"> {location.state.genres.map((genre,index) =>
+                     <li key={index}>{genre}</li>)}</ul> 
+                 <p className="detail__summary">{location.state.summary}</p>
+              </div>
+             </div>
+                
+            </div>)
+                 
+                ;
+        }
+        else return null;
+   }
+}
+
+export default Detail;
